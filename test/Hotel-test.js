4c370fa995c0ccb22fbc2a1d83f3d212c7367192
Nolan Caine <nolancaine@gmail.com>
2021-09-22 22:32:09 -0600

    Add class and test skeletons

---
+++ test/Hotel-test.js
@@ -0,0 +1,12 @@
+import {assert} from 'chai';
+
+import {
+  customers,
+  bookings,
+  rooms
+} from './test-data.js'
+
+import Hotel from '../src/classes/Hotel';
+import Customer from '../src/classes/Customer';
+import Booking from '../src/classes/Booking';
+import Room from '../src/classes/Room';
