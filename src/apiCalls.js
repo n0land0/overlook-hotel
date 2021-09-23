83efae26a4ab2747ffb813f731d6c2d292d44f5a
Nolan Caine <nolancaine@gmail.com>
2021-09-23 00:53:39 -0600

    Write GET functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,21 +1,29 @@
 // GET all customers
 const getAllCustomers = () => {
-
+  return fetch("http://localhost:3001/api/v1/customers")
+  .then(response => response.json)
+  .then(parsed => console.log(parsed));
 }
 
 // GET single customer
-const getOneCustomer = () => {
-
-}
-
-// GET all rooms
-const getAllRooms = () => {
-
+const getOneCustomer = (userID) => {
+  return fetch(`http://localhost:3001/api/v1/customers/${userID}`)
+  .then(response => response.json)
+  .then(parsed => console.log(parsed));
 }
 
 // GET all bookings
 const getAllBookings = () => {
+  return fetch("http://localhost:3001/api/v1/bookings")
+  .then(response => response.json)
+  .then(parsed => console.log(parsed));
+}
 
+// GET all rooms
+const getAllRooms = () => {
+  return fetch("http://localhost:3001/api/v1/rooms")
+  .then(response => response.json)
+  .then(parsed => console.log(parsed));
 }
 
 export {
