a7ab93de91369f033357ef3ac1ef8380b6281faf
Nolan Caine <nolancaine@gmail.com>
2021-09-23 00:46:18 -0600

    Write fxn skeletons

---
+++ src/apiCalls.js
@@ -0,0 +1,26 @@
+// GET all customers
+const getAllCustomers = () => {
+
+}
+
+// GET single customer
+const getOneCustomer = () => {
+
+}
+
+// GET all rooms
+const getAllRooms = () => {
+
+}
+
+// GET all bookings
+const getAllBookings = () => {
+
+}
+
+export {
+  getAllCustomers,
+  getOneCustomer,
+  getAllRooms,
+  getAllBookings
+};
