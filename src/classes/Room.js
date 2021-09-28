493c921ded431b237d536fb7022adf3a66a3f41e
Nolan Caine <nolancaine@gmail.com>
2021-09-27 20:50:56 -0600

    Clean up comments

--- src/classes/Room.js
+++ src/classes/Room.js
@@ -1,9 +1,3 @@
-// import '../assets/1Carly-on-bellcart.png';
-// import '../assets/Dog-KD0C4465a.png';
-// import '../assets/Maud-4-1.png';
-// import '../assets/Pearl_Rooms-Pictures_©James-Bedford-6.png';
-// import '../assets/Roxy-2.png';
-
 class Room {
   constructor(roomObj) {
     this.number = roomObj.number;
@@ -14,7 +8,6 @@ class Room {
     this.costPerNight = roomObj.costPerNight;
     this.bookings = [];
     this.unavailableDates = [];
-    // this.img = "";
   }
 
   populateBookings(bookingsArr) {
@@ -31,23 +24,6 @@ class Room {
     // for now I don't want to actually convert these to Date instances, but the descending-order sort doesn't seem to work otherwise
     ).sort((dateA, dateB) => new Date(dateB) - new Date(dateA));
   }
-
-  // populateImage() {
-  //   switch(this.roomType) {
-  //     case "single room":
-  //       this.img = "../assets/1Carly-on-bellcart.png";
-  //       break;
-  //     case "junior suite":
-  //       this.img = "../assets/Pearl_Rooms-Pictures_©James-Bedford-6.png";
-  //       break;
-  //     case "suite":
-  //       this.img = "../assets/Maud-4-1.png"
-  //       break;
-  //     case "residential suite":
-  //       this.img = "../assets/Roxy-2.png"
-  //       break;
-  //     }
-  // }
 }
 
 export default Room;
